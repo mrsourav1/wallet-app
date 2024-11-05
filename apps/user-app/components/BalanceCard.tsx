1676e656bab38ea0e6c2380ab677624146c178d0
Sourav <souravsahani99@gmail.com>
2024-11-05 16:34:46 +0530

    fixed

--- apps/user-app/components/BalanceCard.tsx
+++ apps/user-app/components/BalanceCard.tsx
@@ -18,6 +18,7 @@ export const BalanceCard = ({amount, locked}: {
                 Total Locked Balance
             </div>
             <div>
+                
                 {locked} INR
             </div>
         </div>
